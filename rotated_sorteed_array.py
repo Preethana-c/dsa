fa5ae31f57537e148383f2e21048194bb6881edf
Preethi <preethana.c@gmail.com>
2026-02-28 20:24:51 +0530

    rotated_sorted_array_done_yayyy

---
+++ rotated_sorteed_array.py
@@ -0,0 +1,30 @@
+def search_rotated_sorted_array(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return 1
+
+    
+        if arr[low] <= arr[mid]:
+            
+            if arr[low] <= target < arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            if arr[mid] < target <= arr[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+    return 0
+
+
+print(search_rotated_sorted_array([4,5,6,7,0,1,2], 0))
+                
+    
+                
