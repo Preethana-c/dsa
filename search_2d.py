f648e5bbfbfc7cbb7d485debc57dab3a813b892b
Preethi <preethana.c@gmail.com>
2026-03-03 18:55:51 +0530

    commit no

--- search_2d.py
+++ search_2d.py
@@ -1,26 +1,35 @@
-def Search_matrix(matrix,target):
-    rows=len(matrix)
-    cols=len(matrix[0])
-    low=0
-    high=rows*cols-1
-    mid_elemeent=matrix[mid_rows][mid_cols]
-    midrows=0
-    midcols=0
-    while low<=high:
-        mid_rows=rows//2
-        mid_cols=cols//2
-        mid_elemeent=matrix[mid_rows][mid_cols]
-        if mid_elemeent==target:
-            return True
-        elif mid_elemeent<target:
-            mid_rows=mid_rows-1
-            mid_cols=mid_cols-1
-        else:
-            mid_rows=mid_rows+1
-            mid_cols=mid_cols+1
-    return False   
+def search_matrix(matrix, target):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    print(rows)
+    print(cols)
 
-Search_matrix([[1,2,3],[4,5,6],[7,8,9]],5)
+    low = 0
+    high = rows * cols - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        row = mid // 2
+        col = mid //2
+
+        mid_element = matrix[row][col]
+
+        if mid_element == target:
+            return True
+        elif mid_element < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return False
+
+
+print(search_matrix([[1,2,3],[4,5,6],[7,8,9]], 0))
+print(search_matrix([[1,2,3],[4,5,6,],[7,8,9],[10,11,12]], 5))
+
+
+
 
     
 
